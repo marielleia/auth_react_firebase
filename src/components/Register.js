7bbe9d148d21963767060a2f85118f233325580b
Marielle <marielle.ibias@example.com>
2022-09-13 01:17:43 +0200

    first commit

---
+++ src/components/Register.js
@@ -0,0 +1,89 @@
+import { useState } from "react";
+import { useAuth } from "../context/authContext";
+import { useNavigate, Link } from "react-router-dom";
+import { Alert } from "./Alert";
+
+export function Register() {
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+
+  const { signup } = useAuth();
+  const navigate = useNavigate();
+  const [error, setError] = useState();
+
+  const handleChange = ({ target: { name, value } }) =>
+    setUser({ ...user, [name]: value });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError("");
+    try {
+      await signup(user.email, user.password);
+      navigate("/");
+    } catch (error) {
+      console.log(error.code);
+      if (error.code === "auth/internal-error") {
+        setError("Correo invalido");
+        // }
+        // if (error.code === "auth/email-already-in-use") {
+        //   setError("Correo en uso. Prueba otro.");
+        // if (error.code === "auth/weak-password") {
+        //   setError("Contraseña débil");
+        //   }
+      }
+      setError(error.message);
+    }
+  };
+
+  return (
+    <div className="w-full max-w-xs m-auto">
+      {error && <Alert message={error} />}
+      <form
+        onSubmit={handleSubmit}
+        className="bg-white shadow-md rounded px-8 py-6 pb-8 mb-4"
+      >
+        <div className="mb-4">
+          <label
+            htmlFor="email"
+            className="block text-gray-700 text-sm font-bold mb-2"
+          >
+            Email
+          </label>
+          <input
+            type="email"
+            name="email"
+            placeholder="youremail@company.ltd"
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            onChange={handleChange}
+          ></input>
+        </div>
+
+        <div className="mb-4">
+          <label
+            htmlFor="password"
+            className="block text-gray-700 text-sm font-bold mb-2"
+          >
+            Password
+          </label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            onChange={handleChange}
+            placeholder="******"
+          ></input>
+        </div>
+
+        <button className="bg-blue-500 hover:bg-blue-700 text-white text-sm font-bold py-2 px-4 rounded focus: outline-none focus:shadow-outline">
+          Register
+        </button>
+      </form>
+      <p className="my-4 text-sm flex justify-between px-3">
+        Do you already have an account? <Link to="/login">Log In</Link>
+      </p>
+    </div>
+  );
+}
